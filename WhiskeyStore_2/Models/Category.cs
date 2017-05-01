34a925e0c29a4676ef5a1d48df7411b90b742ccb
NTRandall <NathanTRandall@gmail.com>
2017-05-01 18:04:33 -0400

    Initial Commit with models

---
+++ WhiskeyStore_2/Models/Category.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WhiskeyStore_2.Models
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        [Display(Name = "Category")]
+        [Required(ErrorMessage = "Category name is required")]
+        [MaxLength(45, ErrorMessage = "The category name can be a maximum 45 characters long")]
+        public string Name { get; set; }
+
+        public virtual ICollection<Products> Products { get; set; }
+    }
+}
