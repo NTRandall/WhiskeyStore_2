36cdf152c48e95c0183671bc1000c2a417652223
NTRandall <NathanTRandall@gmail.com>
2017-05-02 11:25:56 -0400

    Added Checkout controller and Checkout VIews

---
+++ WhiskeyStore_2/Controllers/CheckoutController.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Microsoft.Ajax.Utilities;
+using WhiskeyStore_2.DAL;
+using WhiskeyStore_2.Models;
+
+namespace WhiskeyStore_2.Controllers
+{
+    [Authorize]
+    public class CheckoutController : Controller
+    {
+        private WhiskeyStoreContext db = new WhiskeyStoreContext();
+        const String PromoCode = "Free";
+        public ActionResult AddressAndPayment()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult AddressAndPayment(FormCollection values)
+        {
+            var order = new CustomerOrder();
+
+            TryUpdateModel(order);
+
+            try
+            {
+                if (string.Equals(values["PromoCode"], PromoCode, StringComparison.OrdinalIgnoreCase) == false)
+                {
+                    return View(order);
+                }
+                else
+                {
+                    order.CustomerUserName = User.Identity.Name;
+                    order.DateCreated = DateTime.Now;
+
+                    db.CustomerOrders.Add(order);
+                    db.SaveChanges();
+
+                    var cart = ShoppingCart.GetCart(this.HttpContext);
+                    cart.CreateOrder(order);
+
+                    db.SaveChanges(); //We have received the total amount lets update it
+
+                    return RedirectToAction("Complete", new {id = order.Id});
+                }
+            }
+            catch (Exception ex)
+            {
+                ex.InnerException.ToString();
+                return View(order);
+            }
+        }
+
+        public ActionResult Complete(int id)
+        {
+            bool isValid = db.CustomerOrders.Any(
+                o => o.Id == id &&
+                     o.CustomerUserName == User.Identity.Name);
+
+            if (isValid)
+            {
+                return View(id);
+            }
+            else
+            {
+                return View("Error");
+            }
+        }
+
+    }
+}
