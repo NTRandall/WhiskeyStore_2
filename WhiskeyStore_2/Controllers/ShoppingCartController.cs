2c9cf0d6ba0c612e50722a02eb7ef23c7d5baf1d
NTRandall <NathanTRandall@gmail.com>
2017-05-02 10:19:03 -0400

    Added Shopping Cart View Models and Cart Views

---
+++ WhiskeyStore_2/Controllers/ShoppingCartController.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Services.Description;
+using WhiskeyStore_2.Models;
+using WhiskeyStore_2.ViewModels;
+
+namespace WhiskeyStore_2.Controllers
+{
+    public class ShoppingCartController : Controller
+    {
+        
+        public ActionResult Index()
+        {
+            var cart = ShoppingCart.GetCart(this.HttpContext);
+
+            var viewModel = new ShoppingCartViewModel
+            {
+                CartItems = cart.GetCartItems(),
+                CartTotal = cart.GetTotal()
+            }; 
+
+            return View(viewModel);
+        }
+
+        public ActionResult AddToCart(int id)
+        {
+            var addedProduct = db.Products.Single(product => product.Id == id);
+
+            var cart = ShoppingCart.GetCart(this.HttpContext);
+
+            cart.AddToCart(addedProduct);
+
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        public ActionResult RemoveFromCart(int id)
+        {
+            var cart = ShoppingCart.GetCart(this.HttpContext);
+
+            string productName = db.Carts.FirstOrDefault(item => item.productId == id).Product.Name;
+
+            int itemCount = cart.RemoveFromCart(id);
+
+            var results = new ShoppingCartRemoveViewModel
+            {
+                Message = Server.HtmlEncode(productName) + " has been removed from your shopping cart",
+                CartTotal = cart.GetTotal(),
+                CartCount = cart.GetCount(),
+                ItemCount = itemCount,
+                DeleteId = id
+            };
+            return  Json(results);
+        }
+
+        [ChildActionOnly]
+        public ActionResult CartSummary()
+        {
+            var cart = ShoppingCart.GetCart(this.HttpContext);
+
+            ViewData["CartCount"] = cart.GetCount();
+            return PartialView("CartSummary");
+        }
+    }
+}
